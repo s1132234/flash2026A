53c6313cecd2397bac99f59793e0b05e388575f0
s1132234 <s1132234@pu.edu.tw>
2026-05-12 11:31:25 +0800

    webhook3

--- web.py
+++ web.py
@@ -52,8 +52,12 @@ def webhook():
     req = request.get_json(force=True)
     # fetch queryResult from json
     action =  req["queryResult"]["action"]
-    msg =  req["queryResult"]["queryText"]
-    info = "我是黃士豪設計的電影機器人動作：" + action + "； 查詢內容：" + msg
+    #msg =  req["queryResult"]["queryText"]
+    #info = "我是黃士豪設計的電影機器人，動作：" + action + "； 查詢內容：" + msg
+    if (action == "rateChoice"):
+        rate =  req.get("queryResult").get("parameters").get("rate")
+        info = "我是黃士豪開發的電影聊天機器人,您選擇的電影分級是：" + rate + "，相關電影：\n"
+
     return make_response(jsonify({"fulfillmentText": info}))
 
 
